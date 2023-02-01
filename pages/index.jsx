689905416f659eb94273794d448947a3ba1bd5ee
RyzeXL <dikkayayusuf0@gmail.com>
2023-02-01 14:57:56 +0300

    isa11

--- pages/index.jsx
+++ pages/index.jsx
@@ -10,7 +10,7 @@ export default function Index() {
             <Head>
                 <title>Home | unknowN</title>
             </Head>
-            <Hero />
+           {/*  <Hero /> */}
             <Activities />
             <Repos />
             <Techs />
